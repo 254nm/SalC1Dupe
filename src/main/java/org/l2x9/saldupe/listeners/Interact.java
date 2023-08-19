b45034b786f81fea84362afb50316f2b8b7bdca9
Mason Redacted <67813185+254nm@users.noreply.github.com>
2023-08-19 00:03:05 -0400

    Clean up code

--- src/main/java/org/l2x9/saldupe/listeners/Interact.java
+++ src/main/java/org/l2x9/saldupe/listeners/Interact.java
@@ -3,7 +3,9 @@ package org.l2x9.saldupe.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.*;
+import org.bukkit.entity.ChestedHorse;
+import org.bukkit.entity.Donkey;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
@@ -11,38 +13,35 @@ import org.bukkit.inventory.ItemStack;
 import org.l2x9.saldupe.api.PlayerDupeEvent;
 
 public class Interact implements Listener {
-	@EventHandler
-	public void onVehicleEnter(PlayerInteractAtEntityEvent event) {
-		if (event.getRightClicked() instanceof Llama || event.getRightClicked() instanceof Mule || event.getRightClicked() instanceof Donkey) {
-			if ((event.getPlayer().getInventory().getItemInMainHand().getType() == Material.CHEST) || (event.getPlayer().getInventory().getItemInOffHand().getType() == Material.CHEST)) {
-				if (event.getPlayer().hasPermission("dupe.use")) {
-					ChestedHorse entity = (ChestedHorse) event.getRightClicked();
-					PlayerDupeEvent playerDupeEvent = new PlayerDupeEvent(event.getPlayer(), entity.getLocation().getChunk());
-					Bukkit.getServer().getPluginManager().callEvent(playerDupeEvent);
-					if (!playerDupeEvent.isCancelled()) {
-						if (entity.getPassenger() == null) {
-							entity.setPassenger(event.getPlayer());
-						}
-						event.setCancelled(true);
-						for (ItemStack item : entity.getInventory().getContents()) {
-							if (item != null) {
-								if (!(item.getType() == Material.SADDLE)) {
-									entity.getWorld().dropItemNaturally(entity.getLocation(), item);
+    @EventHandler
+    public void onVehicleEnter(PlayerInteractAtEntityEvent event) {
+        if (!(event.getRightClicked() instanceof ChestedHorse)) return;
+        Player player = event.getPlayer();
 
-								}
-							}
-						}
-						entity.setCarryingChest(false);
-						sendMessage(event.getPlayer(), "&cChests on llamas, donkeys, mules, Etc is currently disabled, this is to facilitate the SalC1 TreeMC dupe");
-					}
-				} else {
-					sendMessage(event.getPlayer(), "&6You do not have permission to dupe");
-				}
-			}
-		}
-	}
+        if (player.getInventory().getItemInMainHand().getType() != Material.CHEST && player.getInventory().getItemInOffHand().getType() != Material.CHEST) return;
 
-	private void sendMessage(Player player, String message) {
-		player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-	}
+        if (!player.hasPermission("dupe.use")) {
+            sendMessage(player, "&6You do not have permission to dupe");
+            return;
+        }
+
+        ChestedHorse horse = (ChestedHorse) event.getRightClicked();
+        if (!horse.getPassengers().contains(player)) horse.addPassenger(player);
+        PlayerDupeEvent playerDupeEvent = new PlayerDupeEvent(player, horse.getLocation().getChunk());
+        Bukkit.getServer().getPluginManager().callEvent(playerDupeEvent);
+
+        if (playerDupeEvent.isCancelled()) return;
+
+        event.setCancelled(true);
+        for (ItemStack item : horse.getInventory().getContents()) {
+            if (item == null || item.getType() == Material.SADDLE) continue;
+            horse.getWorld().dropItemNaturally(horse.getLocation(), item);
+        }
+        horse.setCarryingChest(false);
+        sendMessage(player, "&cChests on llamas, donkeys, mules, Etc is currently disabled, this is to facilitate the SalC1 TreeMC dupe");
+    }
+
+    private void sendMessage(Player player, String message) {
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
 }
